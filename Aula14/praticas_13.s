d03c830b25562ef18fa6a231e64cbe09afce928d
luizokada <luizfernandookada@gmail.com>
2022-11-12 12:39:31 -0300

    Selection and buble sort in the same program

--- Aula14/praticas_13.s
+++ Aula14/praticas_13.s
@@ -1,12 +1,4 @@
-/*
 
-AULA PRATICA
-
-O objetivo do programa eh implementar o algoritmo de ordenação Bubble
-Sort. Nesse metodo, o algoritmo caminha no vetor desordenado, da esquerda para direita, elemento a elemento, comparando cada elemento com o seu vizinho da direita (proximo). Se o elemento proximo for menor que o elemento corrente,
-troca-os de posicao e continua comparando o restante do vetor. Faz isso em todo o vetor desordenado e dessa forma, o maior elemento vai parar na ultima posicao. O elemento da ultima posicao fica na posicao correta de ordenacao. Completa-se entao 1 ciclo. Diminui o tamanho do vetor de 1, para desconsiderar o ultimo elemento que já esta na posicao correta e repete mais um ciclo de operacao com a parte do vetor que ainda está desordenada, completando o segundo ciclo. Repete sucessivamente ateh que a parte do vetor desordenada se reduza a 1 unico elemento (tamanho = 1). Observe que a cada ciclo, o maior elemento é empurrado para o final da parte desordenada do vetor. Nesta técnica de ordenação, se a partir de determinada posicao não ocorrer mais troca, isto significa que a partir da posicao da ultima troca, pra frente, o vetor ja estara ordenado corretamente e o tananho do vetor devera reduzir nao somente 1 posicao, mas para o tamanho do vetor ate a ultima troca. Se precisar relembrar o metodo, leia sobre ele. 
-
-*/
 
 .section .data
 	titulo:  .asciz "\n*** Ordenacao de Vetor por Bubble Sort ***\n\n"
@@ -90,6 +82,15 @@ resolveOpcoes:
 	jmp resolveOpcoes
 
 	_ordenaVetoreSelection:
+	call copiaVetor
+	movl $vetorCopia, %edi
+	call mostravet
+	movl tam, %ecx
+	call ordenaVetorSelection
+	pushl $pulalin
+	call printf
+	addl $4,%esp
+	call mostravet2
 	jmp resolveOpcoes
 
 
@@ -242,15 +243,63 @@ fim:
 	pushl $0
 	call exit
 
-/*
+ordenaVetorSelection:
 
-DESAFIO: Retire o metodo de ordenação Bubble deste programa e insire-o
- como uma funcao no programa que tem o metodo de ordenacao por Selecao, visto na aula passada. 
- Faca um menu de opcoes para o usuario escolher o que deseja fazer (ler vetor, mostra vetor original, mostra vetor ordenado, 
- ordenar por Selecao, ordenar por Bubble, finalizar).
-  O programa deve ficar em loop permanente no menu de opcoes ateh que seja escolhida a opcao de finalizacao. 
-  Observe que o vetor original deve ser preservado sem alteracao e a ordenacao deve ser feita em uma copia. 
-  A mostragem do vetor ordenado mostrara a ultima ordenacao realizada e deve ser feita por uma mesma função, 
-  mas no video deve aparecer o nome do metodo utilizado na ordenacao.
+	movl 	tam, %ecx
+	cmpl 	$1, %ecx
+	jle 	_retorna   	# nao tem oque fazer
 
-*/
+	movl 	$vetorCopia, %edx	# %edx marca a posicao onde sera inserido
+                                # o menor elemento depois de cada busca, que
+		        		# inicialmente é a primeira posicao     
+
+	decl	%ecx	        # %ecx marca a quantidade de elementos 
+                                # seguintes que serao comparados com o menor	
+
+_giroExterno:
+
+	movl	%edx, %edi	# %edi marca a posicao onde esta o menor 
+                        # elemento, que inicialmente esta na                                            
+                        # mesma posicao onde sera inserido o menor
+
+	movl	%edx, %esi	# falta 6
+   
+	addl	$4, %esi        # %esi marca a posicao do proximo a ser 
+                            # comparado com o menor atual 
+
+	pushl 	%ecx         	# backup de %ecx para o loop externo
+
+_giroInterno:
+	movl 	(%edi), %eax    # contem o menor valor
+	movl 	(%esi), %ebx    # contem o valor do proximo
+
+	cmpl 	%eax, %ebx	# compara o menor atual com o proximo
+	jl 	_atualizaPosicaoMenor	# salta se o proximo for menor que
+                                        # o menor atual
+
+_voltaDaAtualizacao:
+	addl 	$4, %esi      # avanca o proximo	# falta 7
+	loop _giroInterno
+
+_trocaElementos:
+	movl 	(%edx), %ebx
+	movl 	(%edi), %eax
+	movl 	%eax, (%edx)
+	movl 	%ebx, (%edi)
+
+	addl 	$4, %edx     # avança a posicao de insercao do menor
+			       # falta 8
+	
+	popl	%ecx
+	loop	_giroExterno
+
+	RET
+
+_atualizaPosicaoMenor:
+	movl 	%esi, %edi      # a posicao do proximo para a ser posicao 
+                                # do menor	# falta 9
+	jmp _voltaDaAtualizacao
+
+_retorna:
+	RET
+
